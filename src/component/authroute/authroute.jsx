03ad91c694d0532e6e76dbd4166ef2a9224192d2
Gorilla-Zhang <841845663@qq.com>
2017-12-12 23:06:01 +0800

    zxt

--- src/component/authroute/authroute.jsx
+++ src/component/authroute/authroute.jsx
@@ -16,8 +16,8 @@ class AuthRoute extends React.Component {
         //获取用户信息
         axios.get('./user/info').
         then(res => {
-            if (res.status == 200) {
-                if (res.data.code == 0) {
+            if (res.status === 200) {
+                if (res.data.code === 0) {
                     //有登陆信息
                 } else {
                     this.props.history.push('/login')
